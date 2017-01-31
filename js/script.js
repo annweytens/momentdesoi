d4be293d9616b01a4bb5b5b56a25fbfef98b3b7f
annweytens <anne-sophie.weytens@hei.yncrea.fr>
2017-01-31 14:51:23 +0100

    V.1.9 Presta

--- js/script.js
+++ js/script.js
@@ -57,4 +57,15 @@ $(document).ready(function(){
         }
     });
   });
+  
+  
+  $(document).ready(function() {
+		$('.js-scrollTo').on('click', function() { // Au clic sur un élément
+			var page = $(this).attr('href'); // Page cible
+			var speed = 900; // Durée de l'animation (en ms)
+			$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+			return false;
+		});
+	});
+  
 })
