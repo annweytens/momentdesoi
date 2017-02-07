f7769fa5d410faa587015b2b2deddd4466fb5bff
annweytens <anne-sophie.weytens@hei.yncrea.fr>
2017-02-07 16:58:22 +0100

    V1.9.8

--- js/script.js
+++ js/script.js
@@ -1,28 +1,3 @@
-//Add Google Maps
-
-var myCenter = new google.maps.LatLng(50.478404, 3.108067);
-
-function initialize() {
-var mapProp = {
-  center:myCenter,
-  zoom:12,
-  scrollwheel:false,
-  draggable:false,
-  mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-
-var marker = new google.maps.Marker({
-  position:myCenter,
-  });
-
-marker.setMap(map);
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
-
 $(document).ready(function(){
   // Add smooth scrolling to all links in navbar + footer link
   $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
@@ -67,5 +42,7 @@ $(document).ready(function(){
 			return false;
 		});
 	});
+	
+	
   
 })
